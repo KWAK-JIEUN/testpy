c873d3defe2062ea4274c2b705ec40813a3d8083
KWAK-JIEUN <102336864+KWAK-JIEUN@users.noreply.github.com>
2022-03-25 12:16:06 +0900

    Add files via upload

---
+++ 3주차 과제11.py
@@ -0,0 +1,184 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+14
+55
+12
+67
+4
+32
+55
+67
+44
+Traceback (most recent call last):
+  File "C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py", line 18, in <module>
+    for H in fr:
+NameError: name 'fr' is not defined
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+Traceback (most recent call last):
+  File "C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py", line 22, in <module>
+    for H in fr:
+NameError: name 'fr' is not defined
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+247
+Traceback (most recent call last):
+  File "C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py", line 25, in <module>
+    for H in fr:
+NameError: name 'fr' is not defined
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+247
+2
+2
+2
+2
+Traceback (most recent call last):
+  File "C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py", line 28, in <module>
+    print(sum(str(H)))
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+247
+2
+2
+2
+2
+Traceback (most recent call last):
+  File "C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py", line 28, in <module>
+    print(sum(str(D)))
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+2
+2
+2
+2
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+32
+55
+67
+44
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+44
+ 
+14
+55
+12
+67
+ 
+4
+ 
+32
+55
+67
+44
+ 
+4
+>>> 
+====== RESTART: C:\Users\82104\Desktop\2-1 학기\파이썬\pythonSource\3주차 과제1.py ======
+Hello
+>>> 
+====== RESTART: C:/Users/82104/AppData/Local/Programs/Python/Python39/w.py =====
+hello
+>>> 
+====================== RESTART: C:/Users/82104/testpy/w.py =====================
+44
+ 
+>>> 
+====== RESTART: C:/Users/82104/Desktop/2-1 학기/파이썬/pythonSource/3주차_과제_1.py =====
+32
+55
+67
+44
+ 
+>>> 
