4acbfe5a0b98b797b1ebf188eb12b7cac9cb4a0e
KWAK-JIEUN <102336864+KWAK-JIEUN@users.noreply.github.com>
2022-04-01 08:52:23 +0900

    Add files via upload

---
+++ 5주차_거꾸로 출력.py
@@ -0,0 +1,9 @@
+words = 'It allows embedding Sage Computations into any Webpage'
+print(len(words))
+print(type(len(words)))
+
+c=len(words)
+while (c>=0):
+    c=c-1
+    print(words[c])
+print('end')
