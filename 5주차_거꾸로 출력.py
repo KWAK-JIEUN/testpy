75f662af4c955ac71766db2a038d060671b65ac9
KWAK-JIEUN <102336864+KWAK-JIEUN@users.noreply.github.com>
2022-04-02 04:11:44 +0900

    Delete 5주차_거꾸로 출력.py

--- 5주차_거꾸로 출력.py
+++
@@ -1,9 +0,0 @@
-words = 'It allows embedding Sage Computations into any Webpage'
-print(len(words))
-print(type(len(words)))
-
-c=len(words)
-while (c>=0):
-    c=c-1
-    print(words[c])
-print('end')
