e885916e8ded1bcebc86df3f53deb0af973d84a6
KWAK-JIEUN <102336864+KWAK-JIEUN@users.noreply.github.com>
2022-04-16 03:10:12 +0900

    Delete 6주차 파이썬 과제.py

--- 6주차 파이썬 과제.py
+++
@@ -1,8 +0,0 @@
-num=[35,10,20,40,90,45]
-print('최솟값   :'+str(min(num)))
-print('최댓값   :'+str(max(num)))
-sum=0
-for k in num :
-    sum=sum+k
-print('총합     :' + str(sum))
-print('평균     :'+str(sum/len(num)))    
